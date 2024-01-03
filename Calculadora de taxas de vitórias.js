a66b12c3d5ad1bc16e6f5918a4cd244290c8fd82
Leandro20Rodrigues <154839952+Leandro20Rodrigues@users.noreply.github.com>
2024-01-03 14:19:39 -0300

    Add files via upload

---
+++ Calculadora de taxas de vitórias.js
@@ -0,0 +1,31 @@
+//taxa de vitórias subtraindo as vitórias com as derrotas.
+function taxaDeVitorias(vitorias, derrotas) {
+    // Calcula o resultado das Rankeadas
+    const totalDeVitorias = vitorias - derrotas;
+  
+    // Verifica o nível de vitórias
+    let nivel;
+  
+    if (vitorias < 10) {
+      nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+    } else if (vitorias >= 100){
+      nivel = "Imortal";
+    }
+  
+    // Retorna o resultado da taxa de vitórias e seu nível.
+    return `O Herói tem um saldo de ${totalDeVitorias} está no nível de ${nivel}`;
+  }
+  
+  // Exemplo de uso da função
+  const totalDeVitorias = taxaDeVitorias(30, 10);
+  console.log(totalDeVitorias);
